e3cd94d52803d3eaf74e4bd35c5102d3fced42a4
michell.lobo <michell.lobo@karibu.cl>
2021-01-03 23:04:55 -0300

    [feature-inicial] nombre de la rama

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -22,7 +22,7 @@ def call(){
                             env.PASO = env.STAGE_NAME;
                             String[] stageArray;
                             String type;
-                            def patternFeature = '^feature-'
+                            def patternFeature = "^feature-"
                             println (env.BRANCH_NAME ==~ patternFeature)
                             /*if (assert env.BRANCH_NAME ==~ patternFeature) {
                                 println "-----------------ENTRO-----------------------";
